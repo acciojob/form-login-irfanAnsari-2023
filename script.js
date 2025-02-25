f847faac1155d8a61cb4b75b7de371366bd08822
Irfan Ansari <123730480+irfanAnsari-2023@users.noreply.github.com>
2025-02-25 07:52:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function getFormvalue() {
-    //Write your code here
+function getFormValue(event){
+    event.preventDefault();
+    let form = document.forms["form1"];
+    let firstName = document.querySelector('[name="fname"]').value.trim()
+    console.log(firstName);
+    let lastName = form["lname"].value.trim();
+    console.log(lastName);
+    if(!firstName || !lastName){
+        alert("Please enter both First Name and Last Name.");
+    }
+    // alert(firstName + " " + lastName);
+    alert(`${firstName} ${lastName}`);
 
+    
 }
